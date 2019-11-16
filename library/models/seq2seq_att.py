92a8968efb936387f69f7d1dbd498005f0d17a06
ahathaway <ahathaway821@gmail.com>
2019-11-15 22:43:14 -0700

    revert changes for first att model

--- library/models/seq2seq_att.py
+++ library/models/seq2seq_att.py
@@ -38,23 +38,32 @@ class Seq2SeqAtt(object):
         self.output_data = None
         self.batch_size = None
 
-    def generate_batch(self):
-        num_batches = len(self.input_word2em_data) // batch_size
-        while True:
-            for batchIdx in range(0, num_batches):
-                start = batchIdx * self.batch_size
-                end = (batchIdx + 1) * self.batch_size
-                encoder_input_data_batch = pad_sequences(self.input_word2em_data[start:end], ds.input_max_seq_length)
-                decoder_target_data_batch = np.zeros(shape=(self.batch_size, ds.target_max_seq_length, ds.num_target_tokens))
-                decoder_input_data_batch = np.zeros(shape=(self.batch_size, ds.target_max_seq_length, ds.num_target_tokens))
-                for lineIdx, target_wid_list in enumerate(self.output_data[start:end]):
-                    for idx, wid in enumerate(target_wid_list):
-                        if wid == 0:  # UNKNOWN
-                            continue
-                        decoder_input_data_batch[lineIdx, idx, wid] = 1
-                        if idx > 0:
-                            decoder_target_data_batch[lineIdx, idx - 1, wid] = 1
-                yield [encoder_input_data_batch, decoder_input_data_batch], decoder_target_data_batch
+def generate_batch(ds, input_data, output_data, batch_size):
+    num_batches = len(input_data) // batch_size
+    while True:
+        for batchIdx in range(0, num_batches):
+            start = batchIdx * batch_size
+            end = (batchIdx + 1) * batch_size
+            encoder_input_paragraph_data_batch = []
+            encoder_input_question_data_batch = []
+            for input_paragraph_data, input_question_data in input_data[start:end]:
+                encoder_input_paragraph_data_batch.append(input_paragraph_data)
+                encoder_input_question_data_batch.append(input_question_data)
+            encoder_input_paragraph_data_batch = pad_sequences(encoder_input_paragraph_data_batch,
+                                                               ds.input_paragraph_max_seq_length)
+            encoder_input_question_data_batch = pad_sequences(encoder_input_question_data_batch,
+                                                              ds.input_question_max_seq_length)
+            decoder_target_data_batch = np.zeros(shape=(batch_size, ds.target_max_seq_length, ds.num_target_tokens))
+            decoder_input_data_batch = np.zeros(shape=(batch_size, ds.target_max_seq_length, ds.num_target_tokens))
+            for lineIdx, target_wid_list in enumerate(output_data[start:end]):
+                for idx, wid in enumerate(target_wid_list):
+                    if wid == 0:  # UNKNOWN
+                        continue
+                    decoder_input_data_batch[lineIdx, idx, wid] = 1
+                    if idx > 0:
+                        decoder_target_data_batch[lineIdx, idx - 1, wid] = 1
+            yield [encoder_input_paragraph_data_batch, encoder_input_question_data_batch,
+                   decoder_input_data_batch], decoder_target_data_batch
 
     @staticmethod
     def get_architecture_file_path(model_dir_path):
@@ -185,16 +194,16 @@ class Seq2SeqAtt(object):
         self.y_test = y_test
         self.batch_size = batch_size
 
-        train_gen = self.generate_batch()
-        test_gen = self.generate_batch(data_set_seq2seq, x_test, y_test, batch_size)
+        train_gen = generate_batch(data_set_seq2seq, x_train, y_train, batch_size)
+        test_gen = generate_batch(data_set_seq2seq, x_test, y_test, batch_size)
 
         train_num_batches = len(x_train) // batch_size
         test_num_batches = len(x_test) // batch_size
 
  
 
-        ds_train = tf.data.Dataset.from_generator(train_gen, ([tf.int64,tf.int64], tf.int64))
-        ds_test = tf.data.Dataset.from_generator(test_gen, ([tf.int64,tf.int64], tf.int64))
+        #ds_train = tf.data.Dataset.from_generator(train_gen, ([tf.int64,tf.int64], tf.int64))
+        #ds_test = tf.data.Dataset.from_generator(test_gen, ([tf.int64,tf.int64], tf.int64))
 
         checkpoint = ModelCheckpoint(filepath=weight_file_path, save_best_only=save_best_only)
 
